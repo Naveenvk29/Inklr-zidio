ab93ca069cc48b8f984a7b63b089be1866b36568
Naveen Vinod Kumar <naveenvinodkumar29@gmail.com>
2025-06-07 22:17:02 +0530

    Add Redux store setup and API slice with base query and endpoints

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,24 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import store from "./redux/store.js";
+import { Provider } from "react-redux";
+import {
+  createBrowserRouter,
+  createRoutesFromChildren,
+  RouterProvider,
+  Route,
+} from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const router = createBrowserRouter(
+  createRoutesFromChildren(<Route path="" element={<App />}></Route>)
+);
+
+createRoot(document.getElementById("root")).render(
+  // <StrictMode>
+  <Provider store={store}>
+    <RouterProvider router={router} />
+  </Provider>
+  // </StrictMode>
+);
---
+++ client/src/redux/constants.js
@@ -0,0 +1,5 @@
+export const BASE_URL = "http://localhost:8080/api/v1";
+export const USERS_URL = `${BASE_URL}/users`;
+export const CATEGORY_URL = `${BASE_URL}/categories`;
+export const BLOG_URL = `${BASE_URL}/blogs`;
+export const COMMENT_URL = `${BASE_URL}/comments`;
